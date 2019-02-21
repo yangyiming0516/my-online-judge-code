68d2f1bc96494f297b0bb210663d76d3453671d1
Yiming Yang <yangyiming0516@163.com>
2019-02-21 21:19:47 +0200

    Create 242.cpp

---
+++ leetcode/242.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> cnt(26);
+        for (int i=0;i<s.length();i++) cnt[s[i]-'a']++;
+        for (int i=0;i<t.length();i++) cnt[t[i]-'a']--;
+        for (int i=0;i<26;i++) if (cnt[i]) return false;
+        return true;
+    }
+};
