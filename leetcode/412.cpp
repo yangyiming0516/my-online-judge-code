d937bce8bf93bcaaffc95c33dc8a0ed6db9a6372
Yiming Yang <yangyiming0516@163.com>
2019-02-21 21:21:56 +0200

    Create 412.cpp

---
+++ leetcode/412.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for (int i=1;i<=n;i++){
+            string tmp = "";
+            if (i%3==0) tmp += "Fizz";
+            if (i%5==0) tmp += "Buzz";
+            if (tmp=="") tmp+= to_string(i);
+            ans.push_back(tmp);
+        }
+        return ans;
+    }
+};
